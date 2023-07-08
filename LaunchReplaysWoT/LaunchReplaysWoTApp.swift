cacef1478808d60e8902991c7c693c3f91b9374b
Wilder Lopez <wlopezelias@gmail.com>
2023-07-08 12:06:55 +0200

    Initial Commit

---
+++ LaunchReplaysWoT/LaunchReplaysWoTApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchReplaysWoTApp.swift
+//  LaunchReplaysWoT
+//
+//  Created by Wilder López on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct LaunchReplaysWoTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
