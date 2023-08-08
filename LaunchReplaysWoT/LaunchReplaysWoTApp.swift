0accce9b606a8bc0b4669e6e05f3137847d95728
Wilder Lopez <wlopezelias@gmail.com>
2023-08-08 13:40:39 +0200

    [ADD] about it view

--- LaunchReplaysWoT/LaunchReplaysWoTApp.swift
+++ LaunchReplaysWoT/LaunchReplaysWoTApp.swift
@@ -14,6 +14,24 @@ struct LaunchReplaysWoTApp: App {
             ContentView()
                 .frame(width: 500, height: 600, alignment: .center)
         }.windowResizabilityContentSize()
+            .commands {
+                CommandGroup(replacing: .appInfo) {
+                    Button("About Launch Replays - WoT") {
+                        NSApplication.shared.orderFrontStandardAboutPanel(
+                            options:
+                            [NSApplication.AboutPanelOptionKey.credits: NSAttributedString(
+                                string: "Made with ❤️ by wle_iGhost",
+                                attributes: [
+                                    NSAttributedString.Key.font: NSFont.boldSystemFont(
+                            ofSize: NSFont.smallSystemFontSize)
+                                ]
+                            ),
+                             NSApplication.AboutPanelOptionKey(rawValue: "Copyright"): "© 2023 Always Blue It Solutions"
+                            ]
+                        )
+                    }
+                }
+            }
     }
 }
 
